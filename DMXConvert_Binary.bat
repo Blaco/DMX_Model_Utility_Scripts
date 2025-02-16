49127cd545025b04607ae77fb1c97aefef75a844
Taco <30645792+Blaco@users.noreply.github.com>
2025-02-16 13:17:05 -0500

    For convenience, not part of tools

--- DMXConvert_Binary.bat
+++ DMXConvert_Binary.bat
@@ -1,3 +1,3 @@
-::This is just a basic script to quickly convert a DMX model to binary format.
+::This is just a basic script to quickly convert a DMX model to binary encoding.
 
 "%VGame%\bin\dmxconvert.exe" -i %1 -of dmx -oe binary -o "%~dpn1.dmx"
